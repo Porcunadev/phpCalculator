4505e692312c33d8851a7a2d22ecf9e7d35daaf6
porcunadev <porcunadev@pop-os.localdomain>
2020-12-15 10:57:34 -0800

    first commit

---
+++ functions.php
@@ -0,0 +1,25 @@
+
+
+<?php
+    $num01 = $_GET["num01"];
+    $oper = $_GET["oper"];
+    $num02 = $_GET["num02"];
+
+    function myCalculator($num01,$oper,$num02) {
+        $sum;
+        switch($oper) {
+            case "add":
+                $sum = $num01 + $num02;
+                break;
+            case "sub";
+                $sum = $num01 - $num02;
+                break;
+            default:
+                    $sum = "Error";
+                break;
+        }
+        return $sum;
+    }
+
+    echo "Value: " . myCalculator($num01,$oper,$num02);
+?>
